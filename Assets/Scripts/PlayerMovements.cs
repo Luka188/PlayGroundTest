c7e08219cbb74d551bdd0561249e16ff9305d12c
Luka188 <lucas.monsegu@epita.fr>
2017-05-19 23:10:06 +0200

    movement++

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -1,30 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class PlayerMovements : MonoBehaviour
 {
     //ints
-
+    
     //floats
     float maxSlope = 50;
     float jump = 0;
+    float defaultFieldOfView;
+    float speedToDecrease = 5;
+    float SpaceCounter = 0;
     public float speed;
     public float MouseSpeed;
     public float JumpForce;
+    public float mySpeed = 5;
    
     //bools
     bool grounded = false;
+    bool Sprinting = false;
+    bool countingSpace;
 
     //Unity Stuffs
     Rigidbody myRigidBody;
     public Camera cam;
-    public AnimationCurve SlopeCurveModifier ;
+    public AnimationCurve curve;
+
+    [SerializeField]
+    Text DisplaySpeed;
+
 
     private void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
+        defaultFieldOfView = cam.fieldOfView;
         
     }
     void Update()
@@ -34,20 +46,30 @@ public class PlayerMovements : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
         CheckJump();
-        
-        float myspeed = SlopeCurveModifier.Evaluate(Pythagore(myRigidBody.velocity.x, myRigidBody.velocity.z));
-
-        if (jump > 0)
+        CheckSprint();
+        if (Sprinting)
         {
-            float prevmagn = Pythagore(myRigidBody.velocity.x,myRigidBody.velocity.z) + 0.5f;
-            myRigidBody.velocity = Vector3.zero;
-            myRigidBody.AddForce(transform.forward * vertical * prevmagn+ Vector3.up * jump, ForceMode.VelocityChange);
-            
+            mySpeed = mySpeed + Time.deltaTime* curve.Evaluate((mySpeed / 15f));
+            Vector3 desiredSpeed = transform.forward * vertical * mySpeed ;
+            Vector3 toadd = new Vector3(desiredSpeed.x - myRigidBody.velocity.x, jump + JumpFormula(), desiredSpeed.z - myRigidBody.velocity.z);
+            myRigidBody.AddForce(toadd, ForceMode.VelocityChange);
         }
         else
         {
-            myRigidBody.AddForce(transform.forward * vertical * myspeed * Time.deltaTime + transform.right * horizontal *myspeed* Time.deltaTime, ForceMode.VelocityChange);
+            mySpeed = mySpeed-(speedToDecrease-5)*Time.deltaTime*4;
+            if (mySpeed < 5)
+            {
+                speedToDecrease = 5;
+                mySpeed = 5;
+            }
+            Vector3 desiredSpeed = transform.forward * vertical * mySpeed  + transform.right * horizontal * mySpeed;
+            Vector3 toadd = new Vector3(desiredSpeed.x - myRigidBody.velocity.x, jump + JumpFormula(), desiredSpeed.z - myRigidBody.velocity.z );
+            myRigidBody.AddForce(toadd, ForceMode.VelocityChange);
         }
+        float currentSpeed = Pythagore(myRigidBody.velocity.x, myRigidBody.velocity.z);
+        DisplaySpeed.text = currentSpeed.ToString("#.##");
+
+        cam.fieldOfView = defaultFieldOfView + (mySpeed>5?mySpeed-5:0) * 2;
         jump = 0.0f;
         
     }
@@ -63,12 +85,46 @@ public class PlayerMovements : MonoBehaviour
     {
         return Mathf.Sqrt(x * x + y * y);
     }
-    
+    float JumpFormula()
+    {
+        if (SpaceCounter == 0)
+            return 0;
+        else
+        {
+            print((1 / (1 + SpaceCounter)));
+            print(SpaceCounter);
+            return (1 / 2 * (1 + SpaceCounter));
+        }
+    }
+
+    void CheckSprint()
+    {
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+            speedToDecrease = mySpeed; 
+        if (Input.GetKey(KeyCode.LeftShift))
+            Sprinting = true;
+
+        else
+        {
+            Sprinting = false;
+        }
+        
+    }
+
     void CheckJump()
     {
         if (Input.GetKeyDown(KeyCode.Space)&&grounded){
             jump = JumpForce;
             grounded = false;
+            countingSpace = true;
+        }
+        if (countingSpace)
+        {
+            SpaceCounter += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            countingSpace = false;
         }
     }
     
